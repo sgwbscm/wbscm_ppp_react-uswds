fdb9b885de5b2224bff29352e19745d152d3c406
sgwbscm <sameer.gore@usda.gov>
2024-12-11 10:59:11 -0500

    testing sbx and devdsl deployment

--- src/main.jsx
+++ src/main.jsx
@@ -9,40 +9,30 @@ import {
 import Root, { loader as rootLoader } from "./App.jsx";
 import { StrictMode } from 'react';
 
-/*
-let hostName = location.hostname;
-console.log(hostName);
-let appPath = "";
-if(hostName.includes("localhost") || hostName.includes("wbscmsbx"))
-{
-  appPath = "wbscm-app-devdsl-ppp-external";
-}*/
 const router = createBrowserRouter([
-  
+
 
   {
     path: "/",
-    element:  <App />,
-    loader:rootLoader,
+    element: <App />,
+    loader: rootLoader,
     children: [
       {
         path: "/:solicitation",
-        element:  <App />,
-  
+        element: <App />,
+
       },
     ]
   },
-    {
-      path: "/ppp",
-    //path: "/"+appPath+"/ppp",
-    element:  <App />,
-    loader:rootLoader,
+  {
+    path: "/ppp",
+    element: <App />,
+    loader: rootLoader,
     children: [
       {
-        //path: "/"+appPath+"/ppp:solicitation",
         path: "/ppp:solicitation",
-        element:  <App />,
-  
+        element: <App />,
+
       },
     ]
   }
@@ -50,8 +40,8 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
- 
+
   <RouterProvider router={router} />
- 
+
 
 )
