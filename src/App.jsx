712200334dc6f769208d8602cadb97d26bec4db3
Sameer Gore <sameer.gore@usda.gov>
2025-01-13 08:38:18 -0500

    updated comments

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ export default function App() {
 
 
     let qServiceURL = null;
-    
+    /*
     console.log(
       'searchSolNum= ' + searchSolNum,
       ' | searchPID= ' + searchPID,
@@ -84,7 +84,7 @@ export default function App() {
       ' | searchSolMethod= ' + searchSolMethod,
       ' | searchLatestionVersion= ' + searchLatestVersion,
       ' | searchPerformanceDate= ' + searchPerformanceDate,
-    );
+    );*/
 
     qServiceURL = qHrefDefault + "?";
     if (searchSolNum != null && searchSolNum.length > 0) {
